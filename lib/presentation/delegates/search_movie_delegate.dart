0d31392ecabf301e90f765f3d47663b25a6f50dc
Max Vigueras <mvt.2000@hotmail.com>
2024-02-11 04:44:47 -0600

    Search Delegate: Redirect movie screen by Search Suggestions

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -51,7 +51,7 @@ class SearchMovieDelegate extends SearchDelegate <Movie?>{
         return ListView.builder(
           itemCount: movies.length,
           itemBuilder: (context, index) {
-            return _MovieItem(movie: movies[index]);        
+            return _MovieItem(movie: movies[index], onMovieSelected: close,);        
           },
         );
       },
@@ -62,8 +62,12 @@ class SearchMovieDelegate extends SearchDelegate <Movie?>{
 class _MovieItem extends StatelessWidget {
 
   final Movie movie;
+  final Function onMovieSelected;
 
-  const _MovieItem({required this.movie});
+  const _MovieItem({
+    required this.movie, 
+    required this.onMovieSelected
+    });
 
   @override
   Widget build(BuildContext context) {
@@ -71,40 +75,43 @@ class _MovieItem extends StatelessWidget {
     final textStyle = Theme.of(context).textTheme;
     final size = MediaQuery.of(context).size;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Row(
-        children: [
-          SizedBox(
-            width: size.width * .2,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.network(movie.posterPath, loadingBuilder: (context, child, loadingProgress) => FadeIn(child: child),
-              )          
+    return GestureDetector(
+      onTap: () => onMovieSelected(context, movie),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          children: [
+            SizedBox(
+              width: size.width * .2,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(movie.posterPath, loadingBuilder: (context, child, loadingProgress) => FadeIn(child: child),
+                )          
+              )
+            ),
+            const SizedBox(width: 10,),
+            SizedBox(
+              width: size.width * 0.7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(movie.title, style: textStyle.titleMedium),
+                  movie.overview.length > 100
+                    ? Text('${movie.overview.substring(0, 100)}...')
+                    : Text(movie.overview),
+                  Row(
+                    children: [
+                      Icon(Icons.star_half_rounded, color: Colors.yellow.shade800,),
+                      const SizedBox(width: 5,),
+                      Text(movie.voteAverage.toString().substring(0,3), 
+                      style: textStyle.bodyMedium!.copyWith(color: Colors.yellow.shade900))
+                    ],
+                  )
+                ],
+              ),  
             )
-          ),
-          const SizedBox(width: 10,),
-          SizedBox(
-            width: size.width * 0.7,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(movie.title, style: textStyle.titleMedium),
-                movie.overview.length > 100
-                  ? Text('${movie.overview.substring(0, 100)}...')
-                  : Text(movie.overview),
-                Row(
-                  children: [
-                    Icon(Icons.star_half_rounded, color: Colors.yellow.shade800,),
-                    const SizedBox(width: 5,),
-                    Text(movie.voteAverage.toString().substring(0,3), 
-                    style: textStyle.bodyMedium!.copyWith(color: Colors.yellow.shade900))
-                  ],
-                )
-              ],
-            ),  
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,5 +1,5 @@
 export 'package:cinemapedia/presentation/widgets/shared/full_screen_loader.dart';
-export 'shared/custom_app.dart';
+export 'shared/custom_appbar.dart';
 export 'movies/movies_slideshow.dart';
 export 'shared/custom_bottom_navigationbar.dart';
 export 'movies/movie_horizontal_listview.dart';
